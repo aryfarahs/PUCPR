4f6136f307522059479e325c45ddc3c8f9df754e
Ary Farah <aryfarahs@gmail.com>
2023-04-28 22:06:15 -0300

    .

--- Rac Algorítmico/ex listas/PARTE 2/17.py
+++ Rac Algorítmico/ex listas/PARTE 2/17.py
@@ -1,7 +1,16 @@
 ordem = ['Primeiro', 'Segundo', 'Terceiro', 'Quarto', 'Quinto']
-nome = str(input('Atleta: '))
+saltos = []
+nome = str(input('Atleta: ')).title()
 for c in range(5):
     salto = float(input(f'{ordem[c]} salto: '))
+    saltos.append(salto)
+print()
 print('Resultado final: ')
 print(f'Atleta: {nome}')
-print(f'Saltos: {}')
+print(f'Saltos: ', end=' ')
+for v, i in enumerate(saltos):
+    if v < len(saltos)-1:
+        print(i, end=' - ')
+    else:
+        print(i)
+print(f'Média: {sum(saltos)/len(saltos)}')
