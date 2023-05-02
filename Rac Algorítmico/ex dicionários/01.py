829ea1cca5c6bc7a70c895a6f8aee37312935449
Ary Farah <aryfarahs@gmail.com>
2023-05-02 08:44:31 -0300

    .

--- Rac Algorítmico/ex dicionários/01.py
+++ Rac Algorítmico/ex dicionários/01.py
@@ -1,10 +1,10 @@
 estoque = {'pao': 10, 'hamburguer': 12, 'tomate': 5, 'bacon': 5, 'ovo': 5}
 cardapio = {
- 'x-burguer': ['pao', 'hamburguer'],
- 'x-salada': ['pao', 'hamburguer', 'tomate'],
- 'x-bacon': ['pao', 'hamburguer', 'tomate', 'bacon'],
- 'x-egg': ['pao', 'hamburguer', 'ovo'],
- 'x-tudo': ['pao', 'hamburguer', 'tomate', 'hamburguer', 'bacon', 'ovo']
+ 'xburguer': ['pao', 'hamburguer'],
+ 'xsalada': ['pao', 'hamburguer', 'tomate'],
+ 'xbacon': ['pao', 'hamburguer', 'tomate', 'bacon'],
+ 'xegg': ['pao', 'hamburguer', 'ovo'],
+ 'xtudo': ['pao', 'hamburguer', 'tomate', 'hamburguer', 'bacon', 'ovo']
 }
 while True:
     cont = 1
@@ -12,18 +12,39 @@ while True:
     print(f'{"CARDÁPIO BOCA FELIZ":^40}')
     print('—'*40)
     for sanduiche, ingredientes in cardapio.items():
-        print(f'[{cont}] {sanduiche.title():<10}  -   ', end='')
+        print(f'[{cont}] {sanduiche[0].title()}-{sanduiche[1:].title():<10}  -    ', end='')
         for c in range(len(ingredientes)):
             print(f'{ingredientes[c].title()}, ', end='')
         print()
         cont += 1
     print('—'*40)
-    ans = input('O que deseja pedir? [0 p/ parar] ').lower()
+    ans = input('O que deseja pedir? [0 p/ parar] ').lower().replace('-', '')
     if ans in '0':
         break
-    elif ans not in sanduiche:
+    elif ans not in cardapio:
         print('\033[1;31mERRO! Item não econtrado.\033[m')
     else:
-
-
-    
+        if ans in 'xburguer' or ans in '1':
+            estoque['pao'] -= 1
+            estoque['hamburguer'] -= 1
+        elif ans in 'xsalada' or ans in '2':
+            estoque['pao'] -= 1
+            estoque['hamburguer'] -= 1
+            estoque['tomate'] -= 1
+        elif ans in 'xbacon' or ans in '3':
+            estoque['pao'] -= 1
+            estoque['hamburguer'] -= 1
+            estoque['tomate'] -= 1
+            estoque['bacon'] -= 1
+        elif ans in 'xegg' or ans in '4':
+            estoque['pao'] -= 1
+            estoque['hamburguer'] -= 1
+            estoque['ovo'] -= 1
+        elif ans in 'xtudo' or ans in '5':
+            estoque['pao'] -= 1
+            estoque['hamburguer'] -= 1
+            estoque['tomate'] -= 1
+            estoque['bacon'] -= 1
+            estoque['ovo'] -= 1                     
+    for ingrediente, quant in estoque:
+        print(f'{ingrediente:<10}{quant:>6}')
