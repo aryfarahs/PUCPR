4748e9b68674cbdb90cd998a843db4764a4c8460
Ary Farah <aryfarahs@gmail.com>
2023-05-06 18:26:13 -0300

    .

--- Mat Computação/TDE 2 - Média.py
+++ Mat Computação/TDE 2 - Média.py
@@ -14,6 +14,8 @@ while True:
     m.clear()
     print('~' * 40)
     typ = int(input('Qual tipo de média deseja calcular? [999 p/ parar] '))
+    if typ == 999:
+        break
     if typ == 3:
         med2 = input('Digite a média pra passar: ').replace(',', '.')
         med = float(med2)
@@ -22,17 +24,36 @@ while True:
             score2 = input(f'Digite a {c+1}ª nota: ').replace(',', '.')
             score = float(score2)
             l.append(score)
-        print('~' * 40)
-        print('( ', end='')
-        for c in range(0, len(l)):
-            if c != len(l) - 1:
-                print(f'{l[c]} + ', end='')
-            elif c == len(l) - 1:
-                print(f'{l[c]} + n ) / {len(l)+1} = 7,0')
-        print(f'n = {med} * {len(l)+1} - {sum(l)}')
-        math = (med * (len(l)+1)) - sum(l)
-        print(f'\033[1;36mPara passar, o aluno deve tirar a nota {math:.1f}\033[m')
-    elif typ > 3 or typ == 0:
+        ans = str(input('Existem pesos? ')).strip().upper()[0]
+        if ans in 'N':
+            print('~' * 40)
+            print('( ', end='')
+            for c in range(0, len(l)):
+                if c != len(l) - 1:
+                    print(f'{l[c]} + ', end='')
+                elif c == len(l) - 1:
+                    print(f'{l[c]} + n) / {len(l)+1} = 7,0')
+            print(f'n = {med} * {len(l)+1} - {sum(l)}')
+            math = (med * (len(l)+1)) - sum(l)
+            print(f'\033[1;36mPara passar, o aluno deve tirar a nota {math:.1f}\033[m')
+        else:
+            for c in range(qnt):
+                peso = int(input(f'Digite o {c+1}º peso: '))
+                p.append(peso)
+            print('( ', end='')
+            for c in range(0, len(l)):
+                if c != len(l) - 1:
+                    mult = l[c]*p[c]
+                    print(f'{mult} + ', end='')
+                    m.append(mult)
+                elif c == len(l) - 1:
+                    mult = l[c]*p[c]
+                    print(f'{l[c]*p[c]} + n x {p[c+1]}) = {sum(p) * med}')
+                    m.append(mult)
+            n = ((sum(p) * med) - sum(m)) / p[len(l)]
+            print(f'n = {sum(p) * med} - {sum(m)} / {p[len(l)]}')
+            print(f'\033[1;36mPara passar, o aluno deve tirar a nota {n:.1f}\033[m')
+    elif typ > 3 or typ == 0 and typ != 999:
         print('\033[1;31mERRO! Digite uma escolha válida.\033[m')
     else:
         num = int(input('Quantas notas quer calcular? '))
@@ -71,8 +92,6 @@ while True:
                 print('Você \033[1;32mPASSOU!\033[m')
             else:
                 print('Você está de \033[1;34mRECUPERAÇÃO!\033[m')
-        elif typ == 999:
-            break
         else:
             print('\033[1;31mERRO! Digite uma escolha válida.\033[m')
 print('–' * 40)
--- TESTES/teste.py
+++
@@ -1,4 +0,0 @@
-g = ['a', 'a', 'a', 'b', 'c', 'v', 'c', 'a']
-f = 'aaabcvca'
-print(g.count('a'))
-print(f.count('a'))
