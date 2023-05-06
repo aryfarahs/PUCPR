74cfdc1e01178529f026a39b0d16f064c3b54736
Ary Farah <aryfarahs@gmail.com>
2023-05-06 02:03:27 -0300

    .

--- Rac Algorítmico/ex funções/09.py
+++ Rac Algorítmico/ex funções/09.py
@@ -1,8 +1,13 @@
 def inverte(num):
-    rev = ''
-    for i in (int(num),-1):
-        rev += i
-    return rev
+    rev = []
+    pos = len(num) - 1
+    for v in num:
+        rev.append(num[pos])
+        pos -= 1
+    for v in rev:
+        print(v, end='')
+    print()
 
 num = str(input('Digite um número: '))
-print(inverte(num))
+print(f'O número invertido fica: ', end='')
+inverte(num)
---
+++ TESTES/teste.py
@@ -0,0 +1,4 @@
+g = ['a', 'a', 'a', 'b', 'c', 'v', 'c', 'a']
+f = 'aaabcvca'
+print(g.count('a'))
+print(f.count('a'))
