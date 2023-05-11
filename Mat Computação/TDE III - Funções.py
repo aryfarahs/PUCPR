158129f3555ee2efa923cec1098f2b41b1c7e86e
Ary Farah <aryfarahs@gmail.com>
2023-05-10 23:38:06 -0300

    .

--- Mat Computação/TDE III - Funções.py
+++ Mat Computação/TDE III - Funções.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from math import sqrt
 
 def formato(x):
     if x % 1 == 0:
@@ -25,6 +26,25 @@ def primeiro(a, b):
     print(f'\033[1;36mA função é {val}!\033[m')
     print(f'Zero da função: {b*-1}/{a} = {(b*-1)/a:.2f}')
 
+def c_delta(a, b, c):
+    return b**2 - 4 * a * c
+
+def raiz(a, b, delta, sinal=1):
+    return (-b + sinal * sqrt(delta)) / 2 * a
+
+def baskhara(a, b, c):
+    delta = c_delta(a, b, c)
+    if delta < 0:
+        print('Nenhuma raíz real encontrada.')
+    elif delta == 0:
+        x = raiz(a, b, delta)
+        print(f'Raiz encontrada: {x}')
+    else:
+        x1 = raiz(a, b, delta)
+        x2 = raiz(a, b, delta, -1)
+        print(f'As raizes da equação são; {x1} e {x2}')
+
+
 
 print('[1] Função do 1º grau\n[2] Função do 2º grau')
 print('—'*40)
@@ -42,8 +62,11 @@ while True:
             grafico(x, y)
 
     elif ans == 2:
-        pass
-    
+        a = float(input('Digite o valor de a: '))
+        b = float(input('Digite o valor de b: '))
+        c = float(input('Digite o valor de c: '))
+        baskhara(a, b, c)
+
     print('—'*40)
     cont = str(input('Deseja continuar? ')).strip().upper()[0]
     if cont in 'N':
