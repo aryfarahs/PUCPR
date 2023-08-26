19f91fc98ea5427e475c24cef0779fc8b99df11d
Ary Farah <aryfarahs@gmail.com>
2023-08-26 18:02:39 -0300

    .

---
+++ 2º Período/Lógica Matemática/Tabela Verdade/values.py
@@ -0,0 +1,19 @@
+def var(quant):
+    if quant == 1:
+        p = ['V','F']
+    if quant == 2:
+        p = ['V','V','F','F']
+        q = ['V','F','V','F']     
+    elif quant == 3:
+        p = ['V','V','V','V','F','F','F','F']  
+        q = ['V','V','F','F','V','V','F','F']
+        r = ['V','F','V','F','V','F','V','F']
+    elif quant == 4:
+        p = ['V','V','V','V','V','V','V','V','F','F','F','F','F','F','F','F']
+        q = ['V','V','V','V','F','F','F','F','V','V','V','V','F','F','F','F']
+        r = ['V','V','F','F','V','V','F','F','V','V','F','F','V','V','F','F']
+        s = ['V','F','V','F','V','F','V','F','V','F','V','F','V','F','V','F']
+
+
+def and_funct(p, q, r='', s=''):
+    print('')
