741941e65162e1e6342c651949d410b9076fccc0
Ary Farah <124756642+aryfarahs@users.noreply.github.com>
2023-08-26 11:37:42 -0300

    .

--- 2º Período/Lógica Matemática/Tabela Verdade/main.py
+++ 2º Período/Lógica Matemática/Tabela Verdade/main.py
@@ -21,8 +21,11 @@ for i in range(0, len(valores)//2):
     valores2.append(false[i])
 
 if quant > 2:
-    for i in range(0, len(valores), quant//2):
-        valores3.append(valores[i])
+    for i in range(0, len(valores)//4):
+        valores3.append(true[i])
+        valores3.append(true[i])
+        valores3.append(false[i])
+        valores3.append(false[i])
 
 print()
 print('\033[1;34m| ', end='')
@@ -34,10 +37,16 @@ print('-'*4*quant)
 
 cont2 = 0
 cont3 = 0
-for c in range(len(valores)):
-    print('| ', end='')
-    print(valores[c], '|', valores2[cont2], ('|', valores3[cont3] if quant > 2 else ''))
-    cont2 += 1
-    cont3 += 1
+if quant == 2:
+    for c in range(len(valores)):
+        print('| ', end='')
+        print(valores[c], '|', valores2[cont2], '|')
+        cont2 += 1
+elif quant == 3:
+    for c in range(len(valores)):
+        print('| ', end='') 
+        print(valores[c], '|', valores3[cont3], '| ' + valores2[cont2] )
+        cont2 += 1
+        cont3 += 1
 
 
