8b6daed27f9d09316b4522456ef8a014e7d6ec2f
Ary Farah <124756642+aryfarahs@users.noreply.github.com>
2023-08-28 11:19:20 -0300

    .

--- 2º Período/Lógica Matemática/Tabela Verdade/main.py
+++ 2º Período/Lógica Matemática/Tabela Verdade/main.py
@@ -1,7 +1,7 @@
 from values import *
 from time import sleep
 
-quant = int(input('Quantas varáveis vai utilizar? (1-4) '))
+quant = int(input('Quantas varáveis vai utilizar (1-4)? '))
 
 print('Montando sua tabela verdade...')
 sleep(2)
