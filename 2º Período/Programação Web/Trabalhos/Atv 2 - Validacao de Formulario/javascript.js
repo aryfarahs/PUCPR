eeda7482999c5ff7c2bac0d8a06aa84d2d2a0145
Ary Farah <aryfarahs@gmail.com>
2023-09-21 23:02:27 -0300

    .

--- 2º Período/Programação Web/Trabalhos/Atv 2 - Validacao de Formulario/javascript.js
+++ 2º Período/Programação Web/Trabalhos/Atv 2 - Validacao de Formulario/javascript.js
@@ -16,13 +16,111 @@ const emailInput = document.getElementById("email")
 form.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    if (nameInput.value === "") {
-        alert("Preencha seu Nome");
+    // nome
+    if (nameInput.value === "" || !isNameValid(nameInput.value)) {
+        alert("Preencha seu nome (apenas letras)");
         return;
     }
+
+    // cpf
+    if (cpfInput.value.length !== 11 || cpfInput.value === !isCpfValid(cpfInput.value)) {
+        alert('CPF Inválido');
+        return;
+    } else {
+       //formatarCpf(cpfInput);
+    }
+
+    // rua
+    if (streetInput.value === "" || !isNameValid(streetInput.value)) {
+        alert("Preencha o campo 'Rua' corretamente (apenas letras)")
+        return;
+    } 
+
+    // cep
+    if (cepInput.value !== 8 || cepInput.value === !isCepValid(cepInput.value)) {
+        alert('CEP Inválido!')
+        return;
+    } else {
+        formatarCep(cepInput)
+    }
+
+    // cidade
+    if (cityInput.value === "" || !isNameValid(cityInput.value)) {
+        alert("Preencha o campo 'Cidade' corretamente (apenas letras)")
+        return;
+    }
+
+    // estado
+    if (stateInput.value.length !== 2 || stateInput.value === !isStateValid(stateInput.value)) {
+        alert("Preencha o campo 'Estado' apenas com a silga")
+        return;
+    } else {
+        stateInput.value.toUpperCase();
+    }
+
+    // telefone
+
+
+    //email
+
+
+    // caso tudo esteja certo:
+    form.submit();
 });
 
-
-function ValidateForm() {
-
+// VALIDANDO CPF
+function formatarCpf(cpfInput) {
+    const tiraLetra = cpfInput.value.replace(/\D/g, '');
+    const cpfPronto = tiraLetra.substring(0, 3) + '.' + tiraLetra.substring(3, 6) + '.' + tiraLetra.substring(6, 9) + '-' + tiraLetra.substring(9, 11);
+    cpfInput.value = cpfPronto
+    return cpfInput.value;
 }
+
+function isCpfValid(cpfInput) {
+    const cpfRegex = new RegExp(
+        /^[\d]{3}\.\d{3}\.\d{3}-\d{2}$/
+    )
+    if (cpfRegex.test(cpfInput)) {
+        return true;
+    } return false;
+}
+
+// VALIDANDO CEP
+function isCepValid(cepInput) {
+    const cepRegex = new RegExp(
+        /^\d{5}-\d{3}$/
+    )
+    if (cepRegex.test(cepInput)) {
+        return true;
+    } return false;
+}
+
+function formatarCep(cepInput) {
+    const tiracep = cepInput.value.replace(/\D/g, '');
+    const cepPronto = tiracep.substring(0, 5) + '-' + tiracep.substring(5, 8);
+    cepInput.value = cepPronto
+    return cepInput.value;
+}
+
+//VALIDANDO RUA, NOME, CIDADE
+function isNameValid(nameInput) {
+    const NameRegex = new RegExp(
+        /^[a-zA-Z\s]+$/
+    )
+    if (NameRegex.test(nameInput)) {
+        return true;
+    } return false;
+}
+
+// VALIDANDO ESTADO
+function isStateValid(stateInput) {
+    const stateRegex = new RegExp(
+        /^[a-zA-Z]{2}$/
+    )
+    if (stateRegex.test(stateInput)) {
+        return true;
+    } return false;
+}
+
+// VALIDANDO TELEFONE
+
