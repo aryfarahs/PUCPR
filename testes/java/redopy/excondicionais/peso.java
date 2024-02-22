ac407e489f6dd12eb888c12a1812b683c88b975e
Ary Farah <124756642+aryfarahs@users.noreply.github.com>
2024-02-22 12:16:40 -0300

    .

---
+++ testes/java/redopy/excondicionais/peso.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+package testes.java.redopy.excondicionais;
+
+    public class peso {
+        public static void main(String[] args) {
+            
+            Scanner sexsc = new Scanner(System.in);
+            System.out.print("Qual seu sexo? [M/F]: ");
+            byte sex = sexsc.nextByte();
+            System.out.println();
+
+            Scanner heightsc = new Scanner(System.in);
+        }
+    }
