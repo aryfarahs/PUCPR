38730585d561c871132ff85bd6a7d6dec5758df5
Ary Farah <124756642+aryfarahs@users.noreply.github.com>
2024-03-06 10:33:26 -0300

    .

--- 3Periodo/POOIntelliJ/Praticando/PersonagemDaTurma/src/Personagem.java
+++ 3Periodo/POOIntelliJ/Praticando/PersonagemDaTurma/src/Personagem.java
@@ -1,2 +1,7 @@
 public class Personagem {
+
+    public Personagem(String nome, String raca, String classe,
+                      String equipamento, int Vida, int mana, int forca) {
+        
+    }
 }
