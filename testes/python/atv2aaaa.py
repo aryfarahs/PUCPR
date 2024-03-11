80b4d1853b52cca3676421b4bf18f131145bf9cc
Ary Farah <124756642+aryfarahs@users.noreply.github.com>
2024-03-11 15:34:59 -0300

    .

---
+++ testes/python/atv2aaaa.py
@@ -0,0 +1,14 @@
+# PYTHON
+x = int(input('Digite o valor de OOA: '))
+y = int(input('Digite o valor de 00B: '))
+z = 0
+
+if x - y < 0:
+   z = y
+else:
+   z = x
+
+print(f'00A: {x} \n'
+      f'00B: {y} \n'
+      f'00C: {z}')
+
