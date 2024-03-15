ff1f1b0d32d69290a88aed0d5df92f2ceae9e8f0
Ary Farah <124756642+aryfarahs@users.noreply.github.com>
2024-03-15 11:55:48 -0300

    .

---
+++ 3Periodo/POO/Praticando/Disciplina/src/Disciplina.java
@@ -0,0 +1,13 @@
+public class Disciplina {
+    private String nome;
+    private Professor professor;
+    private Aluno[] alunos = new Aluno[10];
+
+    public Disciplina(String nome, Professor professor) {
+        this.nome;
+        this.professor = professor;
+    }
+
+
+
+}
