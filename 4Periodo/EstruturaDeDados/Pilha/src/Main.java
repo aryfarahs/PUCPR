0fcbe5073cf1335e3492f8e6330551803a99470a
Ary Farah <124756642+aryfarahs@users.noreply.github.com>
2024-08-09 19:39:22 -0300

    atv stack

---
+++ 4Periodo/EstruturaDeDados/Pilha/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        StaticStack pilha1 = new StaticStack<Integer>(6);
+        StaticStack pilha2 = new StaticStack<String>(4);
+        StaticStack pilha3 = new StaticStack<>(2);
+
+        System.out.println("=== CRIADOR DE PILHA ===");
+
+        pilha1.push(5);
+
+
+    }
+}
---
+++ 4Periodo/EstruturaDeDados/Pilha/src/StaticStack.java
@@ -0,0 +1,52 @@
+public class StaticStack<T> {
+    private int top = -1;
+    private T[] data;
+    private int size;
+
+    public StaticStack(int size) {
+        this.data = (T[]) new Object[size];
+        this.size = size;
+    }
+
+    public void push(T data) {
+        if(isFull()) {
+            throw new IllegalStateException("Pilha cheia!");
+        }
+        this.data[top] = data;
+        this.top++;
+
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Pilha já está vazia!");
+        }
+        T popped = this.data[top];
+        this.data[top] = null;
+        this.top--;
+        return popped;
+    }
+
+    public void clear() {
+        for (int i = this.top; i >= 0; i--) {
+            this.data[i] = null;
+        }
+        this.top = -1;
+    }
+
+    public boolean isFull() {
+        return this.top >= this.size;
+    }
+
+    public boolean isEmpty() {
+        return this.top == -1;
+    }
+
+    public String toString() {
+        
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
