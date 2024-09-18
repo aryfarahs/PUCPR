227e9c196d57711c14de042a8010c215d14915d9
Ary Farah <124756642+aryfarahs@users.noreply.github.com>
2024-09-17 22:37:16 -0300

    Remove()

--- 4Periodo/EstruturaDeDados/BinaryTree/src/Tree.java
+++ 4Periodo/EstruturaDeDados/BinaryTree/src/Tree.java
@@ -10,7 +10,7 @@ public class Tree<T extends Comparable> {
     }
 
 
-    // ADICIONAR NOVO NÚMERO
+    // ADICIONAR NOVO VALOR
     public void add(T value) {
         Node<T> newNode = new Node<T>(value);
         if (root == null) {
@@ -18,9 +18,8 @@ public class Tree<T extends Comparable> {
         } else {
             Node<T> current = this.root;
             while (true) {
-                /*
-                COMPARETO RETORNA:
-                    -1 = menor
+                /* COMPARETO RETORNA:
+                   -1 = menor
                     0 = igual
                     1 = maior
                  */
@@ -43,6 +42,111 @@ public class Tree<T extends Comparable> {
 
         }
 
+    }
+
+    // REMOVER VALOR
+    public boolean rmv(T value) {
+        Node<T> current = this.root;
+        Node<T> currentDad = null;
+
+        // buscar elemento
+        while (current != null) {
+            if (current.getValue().equals(value)) {
+
+                // remover o elemento
+                // filho à direita
+                if (current.getRight() != null) {
+                    Node<T> altern = current.getRight();
+                    Node<T> alternDad = current;
+
+                    // buscar o número mais próximo do que deseja
+                    while (altern.getLeft() != null) {
+                        alternDad = altern;
+                        altern = altern.getLeft();
+                    }
+                    // substituir o atual (com pai)
+                    if (currentDad != null) {
+                        if (current.getValue().compareTo(currentDad.getValue()) == -1) {
+                            currentDad.setLeft(altern);
+
+                        } else {
+                            currentDad.setRight(altern);
+                        }
+                    }
+                    // substituir o atual (sem pai) == é a raiz!
+                    else {
+                        this.root = altern;
+                    }
+
+                    // removendo elemento
+                    if (altern.getValue().compareTo(alternDad.getValue()) == -1) {
+                        alternDad.setLeft(null);
+
+                    } else {
+                        alternDad.setRight(null);
+                    }
+
+                }
+
+                // filho somente à esquerda
+                else if (current.getLeft() != null) {
+                    Node<T> altern = current.getLeft();
+                    Node<T> alternDad = current;
+
+                    // buscar o número mais próximo do que deseja
+                    while (altern.getRight() != null) {
+                        alternDad = altern;
+                        altern = altern.getRight();
+                    }
+                    // substituir o atual (com pai)
+                    if (currentDad != null) {
+                        if (current.getValue().compareTo(currentDad.getValue()) == -1) {
+                            currentDad.setLeft(altern);
+
+                        } else {
+                            currentDad.setRight(altern);
+                        }
+                    }
+                    // substituir o atual (sem pai) == é a raiz!
+                    else {
+                        this.root = altern;
+                    }
+
+                    // removendo elemento
+                    if (altern.getValue().compareTo(alternDad.getValue()) == -1) {
+                        alternDad.setLeft(null);
+
+                    } else {
+                        alternDad.setRight(null);
+                    }
+
+                }
+
+                // sem filho
+                else {
+                    if (currentDad != null) {
+                        if (current.getValue().compareTo(currentDad.getValue()) == -1) {
+                            currentDad.setLeft(null);
+
+                        } else {
+                            currentDad.setRight(null);
+                        }
+                    }
+                    else {
+                        this.root = null
+                    }
+
+                }
+
+            } else if (value.compareTo(current.getValue()) == -1) { // é menor
+                currentDad = current;
+                current = current.getLeft();
+
+            } else {
+                currentDad = current;
+                current = current.getRight();
+            }
+        }
     }
 
     // IMPRIMIR EM-ORDEM
